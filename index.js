51e0b0ebd8836b768ddaf330a4bdc47859b6257f
Matto <matt.mcquain@gmail.com>
2018-10-31 20:18:11 -0700

    finally got to work on lab 1!

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
+var faker = require('faker');
+var randomNumberFirst = faker.random.number();
+var randomNumberSecond = faker.random.number();
+var randomName = faker.name.findName();
+
 console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
