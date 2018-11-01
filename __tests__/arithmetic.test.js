b3d964dae4deb9e370f0d71090bd80de7d9807c7
Matto <matt.mcquain@gmail.com>
2018-10-31 21:40:45 -0700

    added some tests, index.js, and a bit more structure

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,42 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+// Use the faker module to randomize input
+// Test each method for proper use (invoked with number arguments)
+// Test each method for improper use (invoked with one or more non-number arguments)
+
+describe('arithmetic testing', ()=>{
+
+  it('testing our additon', () => {
+    let testAddition = arithmetic.add(2,4);
+    expect(testAddition).toBe(6);
+  });
+
+  it('testing our subtraction', () => {
+    let testSubtraction = arithmetic.subtract(2,4);
+    expect(testSubtraction).toBe(-2);
+  });
+
+  it('testing our multiplication', () => {
+    let testMultiplication = arithmetic.multiply(2,4);
+    expect(testMultiplication).toBe(8);
+  });
+
+  it('testing our multiplication v2', () => {
+    let multiplyNumbers = [2,4,6];
+    let testMultiplication = arithmetic.multiply(...multiplyNumbers); //string this?
+    expect(testMultiplication).toBe(48);
+  });
+
+  it('testing our division', () => {
+    let testDivide = arithmetic.divide(2,4);
+    expect(testDivide).toBe(0.5);
+  });
+
+  it('testing our division v2', () => {
+    let divideNumbers = [6,4,2];
+    let testDivide = arithmetic.divide(...divideNumbers);
+    expect(testDivide).toBe(0.75);
+  });
+});
+
--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const math = require('./lib/arithmetic.js');
 var faker = require('faker');
 var randomNumberFirst = faker.random.number();
 var randomNumberSecond = faker.random.number();
-var randomName = faker.name.findName();
+var randomNameFirst = faker.name.findName();
 
 console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+
+
+console.log(greet(randomNameFirst));
+console.log(math.add(randomNumberFirst, randomNumberSecond));
+console.log(math.subtract(randomNumberFirst, randomNumberSecond));
+console.log(math.multiply(randomNumberFirst, randomNumberSecond));
+console.log(math.divide(randomNumberFirst, randomNumberSecond));
+console.log(greet.hello(randomNameFirst));
